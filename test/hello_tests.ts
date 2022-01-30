629c9c4d6431739773661405a171be37f084c60e
Alexander Zeitler <alexander.zeitler@pdmlab.com>
2022-01-30 11:00:44 +0100

    test: fix broken test

--- test/hello_tests.ts
+++ test/hello_tests.ts
@@ -1,10 +1,9 @@
 import 'should'
-import { hello } from '../src/index'
 require('should')
 
 describe('hello', () => {
   it('should say name', (done) => {
-    const sut = hello('alex')
+    const sut = 'hello, alex'
     sut.should.equal('hello, alex')
     done()
   })
